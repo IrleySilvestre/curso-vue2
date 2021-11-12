ef3958bcc37b4771edffce24aecfa5779b94ab44
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-11-12 20:31:48 -0300

    fim de modulo

--- dom/desafio-05/app.js
+++ dom/desafio-05/app.js
@@ -1,16 +1,18 @@
 new Vue({
-	el: '#desafio',
-	data: {
-		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
-		livro: {
-			titulo: 'O Senhor dos Anéis',
-			autor: 'J.R.R. Tolkiens',
-			volume: '3'
-		},
-		aluno: {
-			id: 10,
-			nome: 'Maria',
-			notas: [7.67, 8.33, 6.98, 9.21]
-		}
-	}
+  el: "#desafio",
+  data: {
+    frutas: ["banana", "maca", "pera", "uva", "laranja", "melao", "morango"],
+    exibDiv: true,
+    array: ["Pedro", "Bia", "Ana", "Rebeca"],
+    livro: {
+      titulo: "O Senhor dos Anéis",
+      autor: "J.R.R. Tolkiens",
+      volume: "3",
+    },
+    aluno: {
+      id: 10,
+      nome: "Maria",
+      notas: [7.67, 8.33, 6.98, 9.21],
+    },
+  },
 });
