a9cc8a96f9b30baa5c028c7305b07ef39cb70905
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-11-22 22:58:46 -0300

    Fim captulo 7

---
+++ vue-cli/component-intro/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-contadores />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/component-intro/src/components/Contador.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="contador">
+    <span>{{contador}}</span>
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.contador++;
+    },
+    sub() {
+      this.contador--;
+    },
+  },
+};
+</script>
+<style>
+.contador {
+  margin-bottom: 10px;
+}
+.contador span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+.contador button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: chocolate;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+</style>
