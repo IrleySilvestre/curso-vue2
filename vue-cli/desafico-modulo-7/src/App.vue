ae1862ba4c9b2955ab58741d79c8ee00e5d121c3
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-11-22 23:25:08 -0300

    Fim desafio modulo 7

---
+++ vue-cli/desafico-modulo-7/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <NavMenu />
+    <Toobar />
+    <Carousel />
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import NavMenu from "./components/NavMenu.vue";
+import Carousel from "./components/Carousel.vue";
+import Toobar from "./components/Toobar.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  name: "App",
+
+  components: { NavMenu, Carousel, Toobar, Footer },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
