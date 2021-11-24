269baf1efa6af73c43ec965e9af374c437c6008b
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-11-23 21:18:49 -0300

    Comunicacao

---
+++ vue-cli/comunicacao/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Usuario/>
+  </div>
+</template>
+
+<script>
+import Usuario from './components/Usuario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Usuario
+  }
+}
+</script>
+
+<style>
+#app {
+ 
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+}
+</style>
