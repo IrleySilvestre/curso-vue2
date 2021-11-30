3fc30202417429b91ea18b13214180ab3e5d89ec
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-11-30 08:07:40 -0300

    todo

---
+++ vue-cli/todo-list/src/components/Todo.vue
@@ -0,0 +1,49 @@
+<template >
+  <div :class="stateClass" class="task" >
+    <p><input @click="checkTask" type="checkbox" name="taskPending" :checked='! task.pending' >{{ task.name }}</p>
+    <div><button>Editar</button> <button @click="removeTask">Excluir</button></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    stateClass() {
+      return {
+        pending: this.task.pending,
+        done: !this.task.pending,
+      };
+    },
+  },
+  methods: {
+      removeTask(){
+          this.$emit('taskRemoved', this.task)
+        },
+        checkTask(){
+            this.$emit('taskModified', this.task)
+        }
+  },
+};
+</script>
+<style scoped >
+.task {
+  box-shadow: 5px 5px 10px 5px #012512;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 15px 0 15px;
+  margin-top: 15px;
+  border-radius: 5px;
+}
+.pending {
+  font-weight:bold; 
+}
+.done {
+  text-decoration-line: line-through;
+}
+</style>
