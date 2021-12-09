6fed612a02c363fc8405b73314c7666cb6c0f670
IrleySilvestre <irleysilvestre.mba@gmail.com>
2021-12-08 21:59:05 -0300

    animation

---
+++ vue-cli/animacoes-transicoes/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button @click="exibir = !exibir" variant="primary" class="mb-3"
+      >Mostrar Mensagem</b-button
+    >
+    <transition name="fade" appear>
+      <b-alert v-if="exibir" variant="info" show>{{ msg }}</b-alert>
+    </transition>
+    <hr />
+    <transition name="slide" appear>
+      <b-alert v-if="exibir" variant="info" show>{{ msg }}</b-alert>
+    </transition>
+
+    <hr />
+    <b-select v-model="tipoAnimacao" class="mb-4">
+      <option value="fade">Fade</option>
+      <option value="slide">Slide</option>
+    </b-select>
+    <transition :name="tipoAnimacao" appear>
+      <b-alert v-if="exibir" variant="info" show>{{ msg }}</b-alert>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Uma mensagem de informação para o usuário",
+      exibir: true,
+      tipoAnimacao: "fade",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(40px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(40px);
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+.slide-leave-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
+}
+</style>
