580ab276757680477232b5091d6c605978af2154
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-01-08 11:07:48 -0300

    validade versao 2

--- frontend/vue/authentication-app/src/components/auth-components/login/Login.js
+++ frontend/vue/authentication-app/src/components/auth-components/login/Login.js
@@ -2,10 +2,31 @@ export default {
   name: "LoginComponent",
   data() {
     return {
-      form:{
-        email:'',
-        password:''
-      }
+      form: {
+        email: "",
+        password: "",
+      },
     };
+  },
+  methods: {
+    validateState(ref) {
+      if (
+        this.veeFields[ref] &&
+        (this.veeFields[ref].dirty || this.veeFields[ref].validated)
+      ) {
+        return !this.veeErrors.has(ref);
+      }
+      return null;
+    },
+
+    onSubmit() {
+      this.$validator.validateAll().then((result) => {
+        if (!result) {
+          return;
+        }
+
+        alert("Form submitted!");
+      });
+    },
   },
 };
--- frontend/vue/authentication-app/src/main.js
+++ frontend/vue/authentication-app/src/main.js
@@ -2,17 +2,21 @@ import Vue from "vue";
 
 import App from "./App.vue";
 import router from "./router";
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VeeValidate from "vee-validate";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.use(VeeValidate, {
+  inject: true,
+  fieldsBagName: "veeFields",
+  errorBagName: "veeErrors",
+});
 
 Vue.config.productionTip = false;
 
