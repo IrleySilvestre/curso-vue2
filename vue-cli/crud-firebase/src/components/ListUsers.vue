f17b2b972b2b52dcf096932dc69b662cddcd8e4a
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-01-12 16:36:40 -0300

    pag cad user

---
+++ vue-cli/crud-firebase/src/components/ListUsers.vue
@@ -0,0 +1,30 @@
+<template>
+     <div>
+          <b-container>
+               <b-table-simple>
+                    <b-thead head-variant="dark">
+                         <b-tr>
+                              <b-th>Nome</b-th>
+                              <b-th>Email</b-th>
+                              <b-th colspan="2">#</b-th>
+                         </b-tr>
+                    </b-thead>
+                    <b-tbody>
+                         <b-tr v-for="(user, index) in users" :key="index">
+                              <b-td>{{ user.name }}</b-td>
+                              <b-td>{{ user.email }}</b-td>
+                              <b-td>Editar</b-td>
+                              <b-td>Excluir</b-td>
+                         </b-tr>
+                    </b-tbody>
+               </b-table-simple>
+          </b-container>
+     </div>
+</template>
+<script>
+export default {
+     name: "ListUsers",
+     props: { users: Array },
+};
+</script>
+<style></style>
---
+++ vue-cli/crud-firebase/src/components/User.vue
@@ -0,0 +1,40 @@
+<template>
+     <div>
+          <h1 class="text-center">Cadastro Usuarios</h1>
+          <hr />
+          <b-container> <div class="bg-warning">Form</div> </b-container>
+
+          <div class="mt-2">
+               <ListUsers :users="users" />
+          </div>
+     </div>
+</template>
+<script>
+import ListUsers from "./ListUsers.vue";
+export default {
+     components: { ListUsers },
+     name: "User",
+     data() {
+          return {
+               form: {
+                    name: "",
+                    email: "",
+               },
+               users: [
+                    { name: "Irley", email: "irleysilvestre.mba@gmail.com" },
+                    { name: "Joaquim", email: "joaquim@gmail.com" },
+               ],
+          };
+     },
+     methods: {
+          addUser() {
+               this.users.push(this.form);
+               this.cleanForm();
+          },
+          cleanForm() {
+               (this.name = ""), (this.email = "");
+          },
+     },
+};
+</script>
+<style></style>
